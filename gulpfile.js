2215c1c9f15f33a5d434bee24e53a267a0d01dae
Bryan Leboff <bleboff@appirio.com>
2017-10-31 10:48:04 -0400

    moved some things around in the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -7,73 +7,35 @@ const gutil = require('gulp-util');
 const path = require('path');
 const colors = gutil.colors;
 const pad = require('pad');
-//Setup CI environment from config/.ci.env
-if(process.env.CI){
-    require('dotenv').config({path: 'config/.ci.env'});
-}
 
 //Setup CI environment from config/.ci.env
 if(process.env.CI){
     require('dotenv').config({path: 'config/.ci.env'});
 }
 
-gulp.task('init', ['auth'], () => {
-    return createScratchOrg()
-    .then(sourcePush)
-    .then(() => assignPermissionSet())
-    .then(() => importData())
-    .then(open);
-});
 
-gulp.task('auth', () =>{
-    return list()
-    .then(getDevHub)
-    .then((devHub) => {
-        //if we have a dev hub, good return it;
-        if(devHub) return Promise.resolve(devHub);
+/****** FIELD CHECKS ********/
+const checkFieldDescription = () => {
+    return validateXML((file) => {
+        let field = JSON.parse(file.contents.toString());
+        if(field.CustomField && !field.CustomField.description){
+            warn('Field', `missing description`, null, path.relative('src', file.path))
+        }
+    });
+}
 
-        //otherwise if we're CI jwtAuth, otherwise webauth a dev hub
-        return process.env.CI ? authJwt() : authWeb();
-    })
-    .then(list);
-});
-gulp.task('create', () => {
-    return createScratchOrg();
-});
+const checkLabelNameMismatch = () => {
+    return validateXML((file) => {
+        let field = JSON.parse(file.contents.toString());
+        let stdLabel = `${field.CustomField.label[0].split(' ').join('_')}__c`;
+        if(field.CustomField && stdLabel != field.CustomField.fullName){
+            warn('Field', `label mismatch`, `found ${colors.yellow(field.CustomField.fullName)} should be ${colors.green(stdLabel)}`, path.relative('src', file.path))
+        }
+    });
+}
 
-gulp.task('assign', () => assignPermissionSet());
-gulp.task('data', () => importData());
-gulp.task('open', () => open());
 
-gulp.task('test', () => runTests())
-
-gulp.task('clean', () => {
-    return list()
-    .then(getScratchOrg)
-    .then(deleteOrg)
-});
-
-gulp.task('pull', () => {
-    return sourcePull();
-});
-
-gulp.task('create', ['auth'] , () => {
-    return createScratchOrg();
-});
-
-gulp.task('check:fields', () => {
-    return gulp.src('**/*.field-meta.xml')
-    .pipe(xml({outType: false}))
-    .pipe(checkFieldDescription())
-    .pipe(checkLabelNameMismatch())
-})
-
-gulp.task('watch', () => {
-    gulp.watch('src/**/*.field-meta.xml', ['check:fields'])
-    gulp.watch('src/**/*.cls', ['push']);
-});
-gulp.task('list', () => list());
-gulp.task('push' , () => sourcePush())
+/****** HELPERS ********/
 
 const authWeb = () => {
     return sfdx.auth.webLogin({
@@ -92,8 +54,7 @@ const authJwt = () => {
     });
 }
 
-const webAuth = () =>
-    sfdx.auth.web()
+const webAuth = () => sfdx.auth.web()
 
 const createScratchOrg = (definitionfile = 'config/project-scratch-def.json', setdefaultusername = true) => {    
         return sfdx.org.create({
@@ -102,24 +63,11 @@ const createScratchOrg = (definitionfile = 'config/project-scratch-def.json', se
         });
 }
 
-const runTests = () => {
-    return sfdx.apex.testRun({
-        'resultformat': 'human'
-    }).then(function(results){
-        if(results.summary.outcome !== 'Passed'){
-            throw new gutil.PluginError('Test Run', {
-                message: 'Tests are failing'
-            });
-        }
-    });
-}
-const sourcePull = () => {
-    return sfdx.source.pull();
-}
+const sourcePull = () => sfdx.source.pull({quiet:false});
 
-const sourcePush = () => {
-    return sfdx.source.push();
-}
+const sourcePush = () => sfdx.source.push({quiet: false});
+
+const list = sfdx.org.list;
 
 const assignPermissionSet = (permsetname = 'DreamHouse') => {
     return sfdx.user.permsetAssign({
@@ -128,17 +76,30 @@ const assignPermissionSet = (permsetname = 'DreamHouse') => {
 }
 const importData = (plan = 'data/sample-data-plan.json') => {
     return sfdx.data.treeImport({
-        'plan': plan
+        'plan': plan,
+        'quiet': false
     });
 }
 
-const open = () =>{
-    return !process.env.CI && sfdx.org.open();
+const runTests = () => {
+    return sfdx.apex.testRun({
+        'resultformat': 'human',
+        'quiet': false
+    }).then(function(results){
+        if(results.summary.outcome !== 'Passed'){
+            throw new gutil.PluginError('Test Run', {
+                message: 'Tests are failing'
+            });
+        }
+    });
 }
 
-const list = () => {
-    return sfdx.org.list();
+
+const open = () =>{
+    return !process.env.CI && sfdx.org.open({quiet: false});
 }
+
+
 const getDevHub = (list) => {
     let devHub = list && list.nonScratchOrgs && list.nonScratchOrgs.filter(function(org){
         return org.isDevHub && org.isDefaultDevHubUsername;
@@ -159,30 +120,12 @@ const deleteOrg = (org) => {
     if(org){
         return sfdx.org.delete({
             'targetusername': org.username,
+            'quiet': false,
             'noprompt': true
         })
     }
 }
 
-
-const checkFieldDescription = () => {
-    return validateXML((file) => {
-        let field = JSON.parse(file.contents.toString());
-        if(field.CustomField && !field.CustomField.description){
-            warn('Field', `missing description`, null, path.relative('src', file.path))
-        }
-    });
-}
-
-const checkLabelNameMismatch = () => {
-    return validateXML((file) => {
-        let field = JSON.parse(file.contents.toString());
-        let stdLabel = `${field.CustomField.label[0].split(' ').join('_')}__c`;
-        if(field.CustomField && stdLabel != field.CustomField.fullName){
-            warn('Field', `label mismatch`, `found ${colors.yellow(field.CustomField.fullName)} should be ${colors.green(stdLabel)}`, path.relative('src', file.path))
-        }
-    });
-}
 
 const warn = (type, subtype, msg, path) => {
     msg = pad(msg||'' ,60,{colors:true});    
@@ -210,3 +153,57 @@ const validateXML = (validator) => {
  
  }
  
+/****** INIT TASKS ********/
+gulp.task('init', ['create', 'init:push', 'init:assign', 'init:import'], open);
+
+gulp.task('create', ['auth'], () => createScratchOrg());
+gulp.task('init:push', ['create'], sourcePush);
+gulp.task('init:assign', ['init:push'], () => assignPermissionSet());
+gulp.task('init:import', ['init:assign'],  () =>  importData());
+
+gulp.task('auth', () =>{
+    return list()
+    .then(getDevHub)
+    .then((devHub) => {
+        //if we have a dev hub, good return it;
+        if(devHub) return Promise.resolve(devHub);
+
+        //otherwise if we're CI jwtAuth, otherwise webauth a dev hub
+        return process.env.CI ? authJwt() : authWeb();
+    })
+    .then(list);
+});
+
+
+/****** DEV TASKS ********/
+gulp.task('assign', () => assignPermissionSet());
+
+gulp.task('data', () => importData());
+
+gulp.task('open', open);
+
+gulp.task('test', runTests)
+
+gulp.task('list', list);
+
+gulp.task('push' , sourcePush);
+
+gulp.task('pull', sourcePull);
+
+gulp.task('clean', () => {
+    return list()
+    .then(getScratchOrg)
+    .then(deleteOrg)
+});
+
+gulp.task('check:fields', () => {
+    return gulp.src('**/*.field-meta.xml')
+    .pipe(xml({outType: false}))
+    .pipe(checkFieldDescription())
+    .pipe(checkLabelNameMismatch())
+})
+
+gulp.task('watch', () => {
+    gulp.watch('src/**/*.field-meta.xml', ['check:fields'])
+    gulp.watch('src/**/*.cls', ['push']);
+});
